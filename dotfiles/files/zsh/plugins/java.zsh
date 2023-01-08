0c8e1fe14f76072debded5f706efac42a105a24f
Tim Co <timco92@gmail.com>
2023-01-08 09:41:57 -0800

    TC - android home export for react native

--- dotfiles/files/zsh/plugins/java.zsh
+++ dotfiles/files/zsh/plugins/java.zsh
@@ -1,3 +1,8 @@
 #THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
 export SDKMAN_DIR="$HOME/.sdkman"
 [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+# Android studio
+export ANDROID_HOME=$HOME/Library/Android/sdk
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/platform-tools
